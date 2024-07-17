7289ae90ef402b4f0026312ecd6fce048e99415a
Kumonda221 <kumonda@kucro3.org>
2024-07-17 16:59:51 +0800

    protocol: all-in-one header for Issue E.b and Issue B

---
+++ chi_b/spec/chi_b_protocol.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef __CHI_B__CHI_PROTOCOL
+#define __CHI_B__CHI_PROTOCOL
+
+#include "chi_b_protocol_flits.hpp"         // IWYU pragma: export
+#include "chi_b_protocol_encoding.hpp"      // IWYU pragma: export
+
+// CHI Protocol Layer headers
+
+#endif // __CHI_B__CHI_PROTOCOL
---
+++ chi_eb/spec/chi_eb_protocol.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef __CHI_EB__CHI_PROTOCOL
+#define __CHI_EB__CHI_PROTOCOL
+
+#include "chi_eb_protocol_flits.hpp"        // IWYU pragma: export
+#include "chi_eb_protocol_encoding.hpp"     // IWYU pragma: export
+
+// CHI Protocol Layer headers
+
+#endif // __CHI_EB__CHI_PROTOCOL
