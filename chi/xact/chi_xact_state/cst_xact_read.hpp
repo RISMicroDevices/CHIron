87294c11f44970eac0eecaecd68bc6a1e93c8da4
Kumonda221 <kumonda@kucro3.org>
2025-09-27 03:19:19 +0800

    fix(chi_xact_state): Errata #1

--- chi/xact/chi_xact_state/cst_xact_read.hpp
+++ chi/xact/chi_xact_state/cst_xact_read.hpp
@@ -39,40 +39,40 @@ namespace CHI {
                     ReadNoSnp_I
                 };
                 // ----------------------------------------------------------------------------------------------------
-                // ReadOnce             | I, UCE        | -          | I     | CompData_UC,   | RespSepData + 
+                // ReadOnce             | I             | -          | I     | CompData_UC,   | RespSepData + 
                 //                      |               |            |       | CompData_I     | DataSepResp_UC
-                constexpr CacheStateTransition ReadOnce_I_UCE_to_I = {
-                    CacheStateTransition(I | UCE, I).TypeRead()
+                constexpr CacheStateTransition ReadOnce_I_to_I = {
+                    CacheStateTransition(I, I).TypeRead()
                         .CompData(I | UC)
                         .DataSepResp(UC)
                 };
                 //
                 constexpr std::array ReadOnce = {
-                    ReadOnce_I_UCE_to_I
+                    ReadOnce_I_to_I
                 };
                 // ----------------------------------------------------------------------------------------------------
-                // ReadOnceCleanInvalid | I, UCE        | -          | I     | CompData_UC,   | RespSepData + 
+                // ReadOnceCleanInvalid | I             | -          | I     | CompData_UC,   | RespSepData + 
                 //                      |               |            |       | CompData_I     | DataSepResp_UC
-                constexpr CacheStateTransition ReadOnceCleanInvalid_I_UCE_to_I = {
-                    CacheStateTransition(I | UCE, I).TypeRead()
+                constexpr CacheStateTransition ReadOnceCleanInvalid_I_to_I = {
+                    CacheStateTransition(I, I).TypeRead()
                         .CompData(I | UC)
                         .DataSepResp(UC)
                 };
                 //
                 constexpr std::array ReadOnceCleanInvalid = {
-                    ReadOnceCleanInvalid_I_UCE_to_I
+                    ReadOnceCleanInvalid_I_to_I
                 };
                 // ----------------------------------------------------------------------------------------------------
-                // ReadOnceMakeInvalid  | I, UCE        | -          | I     | CompData_UC,   | RespSepData + 
+                // ReadOnceMakeInvalid  | I             | -          | I     | CompData_UC,   | RespSepData + 
                 //                      |               |            |       | CompData_I     | DataSepResp_UC
-                constexpr CacheStateTransition ReadOnceMakeInvalid_I_UCE_to_I = {
-                    CacheStateTransition(I | UCE, I).TypeRead()
+                constexpr CacheStateTransition ReadOnceMakeInvalid_I_to_I = {
+                    CacheStateTransition(I, I).TypeRead()
                         .CompData(I | UC)
                         .DataSepResp(UC)
                 };
                 //
                 constexpr std::array ReadOnceMakeInvalid = {
-                    ReadOnceMakeInvalid_I_UCE_to_I
+                    ReadOnceMakeInvalid_I_to_I
                 };
                 // ----------------------------------------------------------------------------------------------------
                 // ReadClean            | I             | -          | SC     | CompData_SC    | RespSepData + 
