67ff929edc7acf5d2fe5a2bb138c2be2f75b81e6
Kumonda221 <kumonda@kucro3.org>
2026-04-27 16:04:19 +0800

    fix(xactions): missing parameters

--- chi/xact/chi_xactions/chi_xactions_impl_HomeWriteZero.hpp
+++ chi/xact/chi_xactions/chi_xactions_impl_HomeWriteZero.hpp
@@ -269,7 +269,7 @@ namespace /*CHI::*/Xact {
     inline XactDenialEnum XactionHomeWriteZero<config>::NextRSPNoRecord(const Global<config>& glbl, const FiredResponseFlit<config>& rspFlit, bool& hasDBID, bool& firstDBID) noexcept
     {
         if (this->IsComplete(glbl))
-            return this->ResponseFlitDenied(XactDenial::DENIED_COMPLETED_RSP;
+            return this->ResponseFlitDenied(XactDenial::DENIED_COMPLETED_RSP, rspFlit);
 
         if (!rspFlit.IsRSP())
             return this->ResponseFlitDenied(XactDenial::DENIED_CHANNEL_NOT_RSP, rspFlit);
