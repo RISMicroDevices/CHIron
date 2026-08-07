d10460c3657e7def9f98b19232d0aee23b22cef9
Kumonda221 <kumonda@kucro3.org>
2026-08-07 17:23:55 +0800

    feat(cchi::xact): collective headers of xactions

---
+++ cchi/xact/cchi_xactions.hpp
@@ -0,0 +1,4 @@
+//#pragma once
+
+#include "cchi_xactions/cchi_xactions_base.hpp"     // IWYU pragma: export
+#include "cchi_xactions/cchi_xactions_impl.hpp"     // IWYU pragma: export
---
+++ cchi/xact/cchi_xactions/cchi_xactions_impl.hpp
@@ -0,0 +1,14 @@
+//#pragma once
+
+#include "cchi_xactions_impl_NonCacheableRead.hpp"              // IWYU pragma: export
+#include "cchi_xactions_impl_CacheableTransientRead.hpp"        // IWYU pragma: export
+#include "cchi_xactions_impl_CacheableAllocatingRead.hpp"       // IWYU pragma: export
+#include "cchi_xactions_impl_Evict.hpp"                         // IWYU pragma: export
+#include "cchi_xactions_impl_WriteBack.hpp"                     // IWYU pragma: export
+#include "cchi_xactions_impl_CacheableDataless.hpp"             // IWYU pragma: export
+#include "cchi_xactions_impl_CMO.hpp"                           // IWYU pragma: export
+#include "cchi_xactions_impl_Stash.hpp"                         // IWYU pragma: export
+#include "cchi_xactions_impl_NonCacheableWrite.hpp"             // IWYU pragma: export
+#include "cchi_xactions_impl_CacheableWrite.hpp"                // IWYU pragma: export
+#include "cchi_xactions_impl_Snoop.hpp"                         // IWYU pragma: export
+#include "cchi_xactions_impl_EvictRemote.hpp"                   // IWYU pragma: export
